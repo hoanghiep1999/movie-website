d127eadc1e60913086df988cd3f6d5767d7acd69
hiep1999hppro <hoanghiep1999hp@gmail.com>
2021-11-17 18:30:16 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
